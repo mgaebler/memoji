cef82d0ed95a47a6a7cf197e0bb3d01b2772b2ec
Marian Gaebler <marian.gaebler@gmail.com>
2020-12-29 12:36:50 +0100

    Add card structure

--- src/screens/Board.tsx
+++ src/screens/Board.tsx
@@ -1,32 +1,42 @@
-import { FC, useCallback, useEffect } from "react";
+import { FC, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Card } from "../domain/Card";
 import { cardReveal, cardsHide, cardsInit } from "../features/game/reducer";
 import { RootState } from "../store";
 import styled from "styled-components";
 
-type ICardContainer = {
+type ICardGrid = {
   items: number;
 };
 
-const CardContainer = styled.div<ICardContainer>(({ items }) => ({
+const CardGrid = styled.div<ICardGrid>(({ items }) => ({
   display: "grid",
-  flexWrap: "wrap",
   gridTemplateColumns: `${100 / items}vh `.repeat(items),
   gridTemplateRows: `${100 / items}vh `.repeat(items),
-  // margin: "50px 10vw",
 }));
 
-const CardTile = styled.div({
+const GridTile = styled.div({
+  // outline: "thin dashed red",
   padding: "12px",
-  margin: "8px",
-  border: "thin solid black",
-  // width: "10vh",
-  // height: "10vh",
-  fontSize: "5rem",
+});
+
+const CardContainer = styled.div({
+  fontSize: "6rem",
+  cursor: "pointer",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
+  height: "100%",
+  width: "100%",
+});
+
+const CardFront = styled(CardContainer)({
+  border: "thin solid gray",
+  borderRadius: 4,
+});
+const CardBack = styled(CardContainer)({
+  border: "thin solid gray",
+  borderRadius: 4,
 });
 
 const Board: FC = () => {
@@ -35,23 +45,28 @@ const Board: FC = () => {
   const items = 4;
   useEffect(() => {
     dispatch(cardsInit());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
       <button onClick={() => dispatch(cardsHide())}>Hide</button>
-      <CardContainer items={items}>
+      <CardGrid items={items}>
         {cards.map((card) => {
           return (
-            <CardTile
-              key={card.id}
-              onClick={() => dispatch(cardReveal({ id: card.id }))}
-            >
-              {card.revealed ? card.icon : "foo"}
-            </CardTile>
+            <GridTile key={card.id}>
+              <CardContainer
+                onClick={() => dispatch(cardReveal({ id: card.id }))}
+              >
+                {card.revealed ? (
+                  <CardFront>{card.icon}</CardFront>
+                ) : (
+                  <CardBack>foo</CardBack>
+                )}
+              </CardContainer>
+            </GridTile>
           );
         })}
-      </CardContainer>
+      </CardGrid>
     </div>
   );
 };
