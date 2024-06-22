c527c624b7dd95139b4234c326e39a6168cdf0f8
Marian Gaebler <marian.gaebler@gmail.com>
2024-06-22 04:41:21 +0000

    feat: add documentation

--- src/functions/randomArrayShuffle.ts
+++ src/functions/randomArrayShuffle.ts
@@ -1,3 +1,11 @@
+/**
+ * Shuffle an array
+ *
+ * The implementation of the randomArrayShuffle function is based on the Fisher-Yates shuffle algorithm,
+ * which randomly permutes the elements of an array. The function iterates over the array and swaps each
+ * element with a random element from the remaining unshuffled portion of the array. This process
+ * continues until all elements have been shuffled.
+ */
 export function randomArrayShuffle<ArrayType>(array: ArrayType[]) {
   let currentIndex: number = array.length;
   let temporaryValue;
