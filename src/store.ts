ba6a7e9a6703b6db969bf7763176a0f4019ae23a
Marian Gaebler <marian.gaebler@gmail.com>
2024-06-23 01:09:33 +0000

    feat: set correct redux hooks

--- src/store.ts
+++ src/store.ts
@@ -5,5 +5,6 @@ import game from "./features/game/gameReducer";
 const store = configureStore({ reducer: { game } });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
